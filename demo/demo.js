a336d21d1d85bc09e2b7c91739f88fce99929bdc
Steven Spriggs <steven.spriggs@gmail.com>
2022-11-15 09:54:37 -0500

    add secondary-nav

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,7 @@
 import '@patternfly/pfe-band';
 import '@patternfly/pfe-cta';
+import '@rhds/elements/rh-secondary-nav/rh-secondary-nav.js';
+
 import '../elements/rh-switcher/rh-switcher.js';
 import '../elements/rh-swap/rh-swap.js';
 import '../elements/rh-feedback-form/rh-feedback-form.js';
