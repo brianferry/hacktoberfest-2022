cfbed59ff1c3d89ada385a4097415dbe26443548
Steven Spriggs <steven.spriggs@gmail.com>
2022-11-15 09:22:04 -0500

    more clean up of demo

---
+++ demo/demo.js
@@ -0,0 +1,5 @@
+import '@patternfly/pfe-band';
+import '@patternfly/pfe-cta';
+import '../elements/rh-switcher/rh-switcher.js';
+import '../elements/rh-swap/rh-swap.js';
+import '../elements/rh-feedback-form/rh-feedback-form.js';
