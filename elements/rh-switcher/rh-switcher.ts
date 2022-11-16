f0617e63e9f0367af3e6ca8b7d3861566ff7c150
Steven Spriggs <steven.spriggs@gmail.com>
2022-11-16 13:29:48 -0500

    persist feedback button for bar and inline

--- elements/rh-switcher/rh-switcher.ts
+++ elements/rh-switcher/rh-switcher.ts
@@ -115,8 +115,12 @@ export class RhSwitcher extends LitElement {
         }
 
         #switch {
-            padding: var(--rh-space-md, 16px) var(--rh-space-md, 16px);
-         }
+            display: flex;
+        }
+
+        :host(:not([variant="inline"])) #switch {
+          padding: var(--rh-space-md, 16px) var(--rh-space-md, 16px);
+        }
 
         :host([variant="card"]) #switch {
             border-block-end: 1px solid var(--rh-switcher-border, var(--rh-color-border-subtle-on-light, #d2d2d2));
@@ -128,7 +132,7 @@ export class RhSwitcher extends LitElement {
         }
 
         :host([variant="inline"]) [part="header"],
-        :host([variant="inline"]) pfe-button {
+        :host([variant="inline"]) pfe-button:not(#feedback) {
             display: none !important;
         }
 
@@ -205,18 +209,18 @@ export class RhSwitcher extends LitElement {
                     <slot></slot>
                 </div>
                 <div id="container">
-                  ${this.variant !== 'card' ? html`
-                    <pfe-button ?hidden="${this._hideFeedback}" id="feedback" @click="${this.#openModal}">
-                      <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" data-icon-name="envelope" height="512" width="512" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"></path></svg>
-                        <span>Feedback</span>
-                      </button>
-                    </pfe-button>
-                  ` : html``}
                     <div id="switch">
                       <pfe-switch id="checked" ?checked="${this._switchChecked}" show-check-icon></pfe-switch>
                       <label for="checked" data-state="on">${this.onMessage}</label> 
                       <label for="checked" data-state="off">${this.offMessage}</label>
+                      ${this.variant !== 'card' ? html`
+                      <pfe-button id="feedback" @click="${this.#openModal}">
+                        <button>
+                          <svg xmlns="http://www.w3.org/2000/svg" data-icon-name="envelope" height="512" width="512" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"></path></svg>
+                          <span>Feedback</span>
+                        </button>
+                      </pfe-button>
+                    ` : html``}
                     </div>
                     <pfe-button plain @click="${this._onCloseClick}">
                       <button>
@@ -251,11 +255,12 @@ export class RhSwitcher extends LitElement {
 
     console.log(storage);
 
-    if (this.variant !== 'card' && !isSwitchChecked && storage.modalToggled !== true) {
-      this._modal.toggle();
-      storage.modalToggled = true;
-      this._hideFeedback = false;
-    }
+    // remove aggressive modal popup
+    // if (this.variant !== 'card' && !isSwitchChecked && storage.modalToggled !== true) {
+    //   // this._modal.toggle();
+    //   storage.modalToggled = true;
+    //   this._hideFeedback = false;
+    // }
 
     storage.switchOn = isSwitchChecked.toString();
 
@@ -284,7 +289,7 @@ export class RhSwitcher extends LitElement {
       const storage = this.#getStorage();
       this._switchChecked = storage.switchOn === 'true';
       this._hideSwitch = storage.hide === 'true';
-      this._hideFeedback = !storage.modalToggled;
+      // this._hideFeedback = !storage.modalToggled;
     }
   }
 
